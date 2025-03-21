5d80f3451d979965515c5845bd6333b871b8b7a4
MarioJunio01 <mario.juniorifpr@gmail.com>
2025-03-20 23:05:18 -0300

    Pagina inicial  criada

---
+++ exercicio1/index.php
@@ -0,0 +1,56 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="template.css">
+    <title>Informações do Instituto Federal </title>
+</head>
+<body class="principal">
+    <h1> Informática</h1>
+    <h2> Curso Tecnico de Informática Integrado ao Ensino Médio</h2>
+    <p>Coordenador:Prof.Dr. Fábio Albini</p>
+    <p>Vice-coodenador:Prof.Msc. Carla Garcia</p>
+    <p>
+    Contato: fabio DOT albini AT ifpr DOT edu DOT br / carla DOT garcia AT ifpr DOT edu DOT br
+
+Para enviar e-mail substituir a palavra DOT por . e a palavra AT por @
+
+(*)A escrita do e-mail desta forma é para não ser possível a aquisição dos endereços de e-mail por intermédio de robôs programados para este fim.
+
+Modalidade: Integrado ao ensino médio – presencial
+
+Requisitos: Ensino fundamental completo
+
+Duração: 4 anos
+
+Turno: Manhã (para ingressantes em anos pares);  tarde (para ingressantes em anos ímpares).
+
+<h2>Projeto Pedagógico:</h2>
+
+<a href="https://ifpr.edu.br/curitiba/wp-content/uploads/sites/11/2020/04/PPC-Informatica_2019-Final-REVISADO.pdf">Projeto Pedagógico 2021 –  Válido para estudantes que ingressarem a partir do ano de 2021.</a>
+
+<h2>Apresentação do Curso:</h2>
+
+O curso Técnico em Informática tem por objetivo a formação de profissionais em nível médio para atuar no mercado de trabalho nas diversas áreas de informática, com especificidade no desenvolvimento de sistemas computacionais.
+
+Conforme a proposta educacional da instituição objetiva-se também uma formação humanística e integral para que além de técnicos, os profissionais sejam cidadãos críticos e reflexivos capazes de compreender e atuar em sua realidade, explorando o uso das tecnologias com responsabilidade social.
+
+<h2>Objetivos:</h2>
+
+Desenvolver e operar sistemas, aplicações e interfaces gráficas;
+
+Organizar a coleta e documentação de informações sobre o desenvolvimento de projetos;
+
+Desenvolver programas de computadores, integrado à tecnologia de banco de dados, rede de computadores e web;
+
+Aplicar linguagens e ambientes de programação no desenvolvimento de software;
+
+Selecionar programas de aplicação a partir da avaliação das necessidades do usuário;
+
+Identificar o funcionamento e relacionamento entre os componentes de computadores e seus periféricos;Identificar os serviços de sistemas operacionais e de rede.
+    </p>
+
+
+</body>
+
+</html>
