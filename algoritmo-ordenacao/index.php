b0b6e216d735a9930942f92d20686054f5be7d28
MarioJunio01 <mario.juniorifpr@gmail.com>
2025-05-30 21:00:12 -0300

    Projeto Polar Iniciado

--- algoritmo-ordenacao/index.php
+++ algoritmo-ordenacao/index.php
@@ -17,15 +17,22 @@
   for($i=0;$i<10;$i++){
     echo "<tr>\n";
     echo "<td>$i</td>\n";
-    //echo "<td><input type=\"text\" name=\"elemento[]\" value=\"0\"></td>\n";
+
     echo "<td><input type=\"number\" name=\"elemento[]\"></td>\n";
     echo "</tr>\n\n";
   }
   ?>
 </table>
-<?php
+<div>
+<label for="crescente">crescente</label><input type="radio" name="crescente" id="crescente" value="1">
 
-?>
+</div>
+<div>
+<label for="decrescente">decrescente</label> <input type="radio" id="decrescente" name="decrescente" value="2">
+
+</div>
+
+
 <input type="submit">
 </form>
 </body>
--- algoritmo-ordenacao/ordenacao.php
+++ algoritmo-ordenacao/ordenacao.php
@@ -1,20 +1,22 @@
 <html>
-    link
+    
 <head>
 <title>Exercício de revisão de PHP (ordenação)</title>
 </head>
 <body>
 
 <?php
+ 
+ 
     $i = 0;
     foreach ($_POST['elemento'] as $value){
         $vet[$i] = $value;
         $i++;
     } 
-    echo "O vetor desordenado está da seguinte forma: <br><br>";
-    for($i=0;$i<10;$i++){
-        echo "elemento[$i] = $vet[$i]<br>";
-    }
+   
+    
+if ($_POST['crescente']==1) {
+    # code...
 
     for($i=0;$i<10;$i++){
         for($j=0;$j<10-1-$i;$j++){
@@ -25,10 +27,29 @@
             }
         }
     }
-    echo "<br><br>O vetor ordenado ficou da seguinte forma: <br><br>";
+    echo "<br><br>O vetor ordenado  de forma crescente ficou da seguinte forma: <br><br>";
     for($i=0;$i<10;$i++){
         echo "elemento[$i] = $vet[$i]<br>";
     }
+}
+else if ( $_POST['decrescente']==2) {
+    # code...
+
+    for($i=0;$i<10;$i++){
+        for($j=0;$j<10-1-$i;$j++){
+            if($vet[$j]<$vet[$j+1]){
+                $swp=$vet[$j];
+                $vet[$j]=$vet[$j+1];
+                $vet[$j+1]=$swp;
+            }
+        }
+    }
+    echo "<br><br>O vetor ordenado  de forma decrescente ficou da seguinte forma: <br><br>";
+    for($i=0;$i<10;$i++){
+        echo "elemento[$i] = $vet[$i]<br>";
+    }
+}
+
 ?>
 
 </body>
